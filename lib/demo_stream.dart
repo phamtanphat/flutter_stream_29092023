994f0743a57d9e58dfaa5f9935d5ce43ed031104
phamtanphat <phatdroid94@gmail.com>
2023-11-10 20:33:28 +0700

    create stream

--- lib/demo_stream.dart
+++ lib/demo_stream.dart
@@ -15,9 +15,22 @@ import 'package:flutter/material.dart';
 
 void main() {
   // 1: Khoi tao bang Future
-  final Future<String> futureString = Future.delayed(
-      Duration(seconds: 1), () => "Hello");
-  Stream<String> streamFuture = Stream.fromFuture(futureString);
+  // final Future<String> futureString = Future.delayed(
+  //     Duration(seconds: 1), () => "Hello");
+  // Stream<String> streamFuture = Stream.fromFuture(futureString);
+  //
+  // streamFuture.listen((event) { print(event); });
 
-  streamFuture.listen((event) { print(event); });
+  // 2: Khoi tao bang iterable
+  // Stream<int> streamIterable = Stream.fromIterable([10, 20, 30, 40, 50]);
+  //
+  // streamIterable.listen((event) { print(event); });
+
+  // 3: Khoi tao bang periodic
+  Stream<int> streamNumber =
+      Stream.periodic(Duration(seconds: 1), (count) => count);
+
+  streamNumber.listen((event) {
+    print(event);
+  });
 }
